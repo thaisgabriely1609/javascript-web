ea9082e2ed9d65376ebee34588639b2cdaab0514
Thais Gabriely <105493307+thaisgabriely1609@users.noreply.github.com>
2023-10-10 08:21:33 -0300

    Add files via upload

---
+++ JS main.js
@@ -0,0 +1,63 @@
+//Pom---------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.queryseloector('tecla_pom').onclick= tocaSomPom();
+
+/*
+    Clap
+*/
+funnction tocaSomPom(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap'). onclick=tocaSomPom;
+
+/*
+    Tim
+*/
+function tocaSomPomTim()[
+  document.queryselector('#som_tecla_tim').play();   
+
+
+  document.queryselector('.tecla_tim').onclick= tocasomClap;
+
+
+  function tocasomPuff(){
+     document.querySelector('som_tecla_puff').play();
+  }
+
+  document.querySelector('.tecla_clap').onclick= tocasomClap;
+
+  funcion tocaSomSlapsh(){
+     document.querySelector('#som_tecla_clap').play();
+  }
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla-clap'). play();
+}
+
+document.querySelector(.tecla_clap).onclik = tocaSomClap;
+
+  function tocaSomPsh(){
+    document.querySelector('.#som_tecla_psh').play();
+  }
+
+  document.querySelector('.tecla_psh').onclick= tocaSomClap;
+
+
+  function tocaSompsh(){
+    document.querySelector('#som_tecla_psh').play();
+  }
+
+  document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+
+  function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+  }
+
+  document.querySelector('.tecla_tom').onclick = tocaSomTom;
